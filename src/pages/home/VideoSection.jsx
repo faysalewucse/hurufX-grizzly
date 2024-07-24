def8e1cb4961994a19f5373eaca9f9acea65f716
faysalewucse <faysal.ewucse@gmail.com>
2024-07-24 23:44:46 +0600

    pull from main

--- src/pages/home/VideoSection.jsx
+++ src/pages/home/VideoSection.jsx
@@ -3,18 +3,17 @@ import play from "../../assets/play.svg";
 const VideoSection = () => {
   return (
     <div className="flex md:flex-row flex-col gap-5 md:pl-20 md:py-20 py-5">
-      <div className="text-white flex-1">
-        <h1 className="md:text-[56px]">web design Bristol</h1>
-        <div className="btn-group text-white">
-          <a
-            className="btn-primary"
-            href="https://www.wearegrizzly.com/web-design-bristol/"
-            target="_self"
-            data-cmp-ab="2"
-          >
-            <span className="btn-circle bg-white"></span>{" "}
-            <span className="btn-label">Web Design Bristol</span>{" "}
-          </a>
+      <div className="text-white flex-1 flex items-center pl-20">
+        <div>
+          <h1 className="text-[56px]">web design</h1>
+          <h1 className="text-[56px]">Bristol</h1>
+          <div className="group/item flex items-center cursor-pointer">
+            <div className="w-10 h-10 bg-white rounded-full group-hover/item:w-56 transition-all duration-500"></div>
+            <hr className="group-hover/item:opacity-0 opacity-100 transition-all duration-500 border-secondary w-10" />
+            <p className="group-hover/item:-ml-56 group-hover/item:text-black transition-all duration-500 ml-5">
+              Web Design Bristol
+            </p>
+          </div>
         </div>
       </div>
       <div className="relative md:w-1/2">
